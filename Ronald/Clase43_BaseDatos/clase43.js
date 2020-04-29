eceb93df49037d8ca7fa70837ffda771ad1043c7
Ronald <ronystivens@hotmail.com>
2020-04-29 16:41:45 +0200

    eliminando comentarios

--- Ronald/Clase43_BaseDatos/clase43.js
+++ Ronald/Clase43_BaseDatos/clase43.js
@@ -1,5 +1,3 @@
-
-// Your web app's Firebase configuration
 var firebaseConfig = {
     apiKey: "",
     authDomain: "peliculas-f276d.firebaseapp.com",
@@ -12,8 +10,8 @@ var firebaseConfig = {
   // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 let ref = firebase.database().ref();
-ref.on("value", function(snapshot){   //Imprimo en el DOM la peliculas que están ya en la base de datos
-    snapshot.forEach(function(childsnapshot){ //el forEach es necesario para que recorra cada objeto y poder meterme en un valor de cada objeto
+ref.on("value", function(snapshot){   //Imprimo en DOM
+    snapshot.forEach(function(childsnapshot){ 
         console.log(childsnapshot.val().Title);
         document.getElementById("busquedas").innerHTML += " ";
         document.getElementById("busquedas").innerHTML += childsnapshot.val().Title+"<br>";
@@ -21,26 +19,24 @@ ref.on("value", function(snapshot){   //Imprimo en el DOM la peliculas que está
 });
 
 document.getElementById("enviar").addEventListener("click",function(){
-    let peticion = document.getElementById("pelicula").value.toLowerCase(); //Cojo el input y lo paso a minusculas para que coincida con la base de datos que lo tendré todo en minusculas
+    let peticion = document.getElementById("pelicula").value.toLowerCase(); 
     let ref = firebase.database().ref(peticion);
     let previas = document.getElementById("busquedas").innerText;
 
-    let expresionRegular = new RegExp(peticion, "gi"); //creo una regexp que incluya el input y detecte si coincide con las peliculas que están ya almacenadas en la base de datos
+    let expresionRegular = new RegExp(peticion, "gi"); 
     if(expresionRegular.test(previas)){
         console.log("pelicula repetida");
 
         ref.on("value",function(snapshot){
             console.log(snapshot.val());
-            document.getElementById("contenido").innerText= " "; //Para que me limpie el DOM de alguna busqueda previa
-                    //document.getElementById("busquedas").innerText= " "; //Para que me limpie el DOM de las anteriores values sino se me añade los nuevos values con los anteriores que ya estaban en el DOM
+            document.getElementById("contenido").innerText= " "; 
                     let data = snapshot.val();
-                    for (let clave in data){ //recorro el json 
-                        //console.log(typeof(data[clave]));
-                        if (typeof(data[clave]) === `string` ){ //Para eliminar la escritura de objetos en el dom, y que solo me escriba cadenas string. 
+                    for (let clave in data){  
+                        if (typeof(data[clave]) === `string` ){ 
                             document.getElementById("contenido").innerHTML += `${clave}:${data[clave]}<br>`;            
                         }   
                     }; 
-                    document.getElementById("poster").setAttribute("src",data.Poster); //saco el poster de la película
+                    document.getElementById("poster").setAttribute("src",data.Poster); 
         });
 
     }else{
@@ -51,21 +47,19 @@ document.getElementById("enviar").addEventListener("click",function(){
                    return response.json();
                 }) 
                 .then(function(data){
-                    //console.log(data)
-                    document.getElementById("contenido").innerText= " "; //Para que me limpie el DOM de alguna busqueda previa
-                    document.getElementById("busquedas").innerText= " "; //Para que me limpie el DOM de las anteriores values sino se me añade los nuevos values con los anteriores que ya estaban en el DOM
-                    for (let clave in data){ //recorro el json 
-                        if (typeof(data[clave]) === `string` ){ //Para eliminar la escritura de objetos en el dom, y que solo me escriba cadenas string. 
+                    document.getElementById("contenido").innerText= " "; 
+                    document.getElementById("busquedas").innerText= " "; 
+                    for (let clave in data){ 
+                        if (typeof(data[clave]) === `string` ){ 
                             document.getElementById("contenido").innerHTML += `${clave}:${data[clave]}<br>`;            
                         }   
                     }; 
-                    document.getElementById("poster").setAttribute("src",data.Poster); //saco el poster de la película  
-                    //Para prevenir que no se suban a la base de datos, las peliculas que no se encuentran  pero que dan igualmente un json de respuesta.
+                    document.getElementById("poster").setAttribute("src",data.Poster);   
                     if(data.Response == "False"){
                         console.log("no se sube esta pelicula");
                         document.getElementById("busquedas").innerHTML = "Vuelve a probar con otro nombre!!";                      
                     }else{
-                        ref.set(data); //aqui sí se sube la pelicula
+                        ref.set(data); 
                     }  
                     
                 })
@@ -77,14 +71,14 @@ document.getElementById("enviar").addEventListener("click",function(){
 });
 
 
-//Para borrar una pelicula de la base de datos.
+// borrar pelicula
 document.getElementById("enviarBorrar").addEventListener("click",function(){
      let pelicula = document.getElementById("peliculaBorrar").value.toLowerCase();
      let previas = document.getElementById("busquedas").innerText; 
-     let expresionRegular = new RegExp(pelicula, "gi"); //le paso una expresion regular para que solo pueda leer una pelicula que esté en la base de datos y no cualquier input.
+     let expresionRegular = new RegExp(pelicula, "gi"); 
      if(expresionRegular.test(previas)){
-     document.getElementById("busquedas").innerText= " "; //limpia previamente el dom para que no se sobreescriba al generarse el nuevo evento.
-     firebase.database().ref(pelicula).remove(); //elimina la pelicula
+     document.getElementById("busquedas").innerText= " "; 
+     firebase.database().ref(pelicula).remove(); 
      };
     
 });
--- Ronald/Clase44_Autentificacion/clase44.js
+++ Ronald/Clase44_Autentificacion/clase44.js
@@ -7,52 +7,50 @@ var firebaseConfig = {
     messagingSenderId: "715185555678",
     appId: "1:715185555678:web:0e10b0e8e87b6572fd9a4f"
   };
-  // Initialize Firebase
+// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-//Para mostrar en el DOM los usuarios ya registrados previamente y almacenados en la base de datos
+//Mostrar en el DOM 
 let ref = firebase.database().ref("usuariosRegistrados"); 
 ref.on("value",function(snapshot){
-  document.getElementById("usuariosRegistrados").innerHTML = ""; //Me limpia el DOM de los anteriores datos para que no se escriban dos veces los usurios
-  snapshot.forEach(function(childSnapshot){ //recorro los usuarios(los hijos de usuariosRegistrados)
+  document.getElementById("usuariosRegistrados").innerHTML = ""; 
+  snapshot.forEach(function(childSnapshot){ 
     document.getElementById("usuariosRegistrados").innerHTML += `${childSnapshot.val().name} ${childSnapshot.val().surname}<br>`
     console.log("usuarios registrados en el sistema:",childSnapshot.val());
   })
 });
 
-document.getElementById("acceso").style.display = "none"; //Permanece oculto para nuevos usuarios
+document.getElementById("acceso").style.display = "none"; 
 
 
-//Acceso mediante github
+//github
 document.getElementById("github").addEventListener("click",function(){
   var provider = new firebase.auth.GithubAuthProvider();
   firebase.auth().signInWithPopup(provider).then(function(result) {
-    var token = result.credential.accessToken;// This gives you a GitHub Access Token. You can use it to access the GitHub API
-    var user = result.user;// The signed-in user info.
+    var token = result.credential.accessToken;
+    var user = result.user;
     console.log(user);
-  }).catch(function(error) { // Handle Errors here.
+  }).catch(function(error) { 
     var errorCode = error.code;
     var errorMessage = error.message;
-    var email = error.email;// The email of the user's account used.
-    var credential = error.credential;// The firebase.auth.AuthCredential type that was used.
+    var email = error.email;
+    var credential = error.credential;
     console.log(errorMessage);
     document.getElementById("validacion").innerText = errorCode;
   }); 
 });
 
-//Para ver los usuarios conectados o registrados
+//Para usuarios conectados o registrados
 firebase.auth().onAuthStateChanged(function(user) {  
   if (user) {
-    // User is signed in.
     console.log(`el ususario ${user.email} está conectado`)
     document.getElementById("usuariosOnline").innerText = `${user.email} esta conectado`;
   } else {
-    // No user is signed in.
     console.log(`Ningun usuario logueado`)
   }
 });
 
-//Para usuarios ya registrados, despliega el formulario de acceso
+//Para usuarios ya registrados, 
 document.getElementById("boton").addEventListener("click", accesoRegistrado); 
 function accesoRegistrado(){
   document.getElementById("registro").style.display = "none";
@@ -60,7 +58,7 @@ function accesoRegistrado(){
   document.getElementById("boton").style.display = "none";
 }
 
-//Registro de usuario:
+//Registro:
 document.getElementById("enviar").addEventListener("click", registro);
 function registro(){
   let nombre = document.getElementById("nombre").value;
@@ -73,22 +71,21 @@ function registro(){
     console.log(errorCode+"("+errorMessage+")");
     document.getElementById("validacion").innerText = errorCode;
   });
-  let ref = firebase.database().ref("usuariosRegistrados/"+nombre); //accedo a la base de datos para registrar el nuevo usuario.
-  let datos = { //recopilo los datos del nuevo usuario en un objeto 
+  let ref = firebase.database().ref("usuariosRegistrados/"+nombre); //registro el nuevo usuario.
+  let datos = { 
     name: nombre,
     surname: apellido,
     email: email
     }
-  ref.set(datos); //registro el nuevo usuario en la base de datos.  
+  ref.set(datos);  
 }
 
-//Acceso, log in de usuarios ya registrados.
+// log in
 document.getElementById("enviar2").addEventListener("click", acceso);
 function acceso(){
     let email2 = document.getElementById("correo2").value;
     let password2 = document.getElementById("contraseña2").value;
     firebase.auth().signInWithEmailAndPassword(email2, password2).catch(function(error) {
-      // Handle Errors here.
       var errorCode = error.code;
       var errorMessage = error.message;
       console.log(error.code);
@@ -100,23 +97,23 @@ function acceso(){
     });
 };
 
-//Log out de usuarios que se acaban de registrar.
+//Log out 
 document.getElementById("salir").addEventListener("click", salir);
 function salir(){
     firebase.auth().signOut().then(function() {
-      console.log("Deslogado") // Sign-out successful.
+      console.log("Deslogado") 
       document.getElementById("usuariosOnline").innerHTML = `Desconectado`;
     }).catch(function(error) {
-        console.log("Error en el signOut") // An error happened.
+        console.log("Error en el signOut") 
     });
 };
-//Log out de usuarios ya registrados (en la ventana exclusiva de usuarios ya registardos)  
+//Log out ( ventana exclusiva de usuarios ya registardos)  
 document.getElementById("salir2").addEventListener("click", salir2);
 function salir2(){
     firebase.auth().signOut().then(function() {
-      console.log("Deslogado") // Sign-out successful.
+      console.log("Deslogado") 
       document.getElementById("usuariosOnline").innerHTML = `Desconectado`;
     }).catch(function(error) {
-        console.log("Error en el SignOut") // An error happened.
+        console.log("Error en el SignOut") 
     });
 };
