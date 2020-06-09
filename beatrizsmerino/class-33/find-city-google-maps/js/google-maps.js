c7bccb9932a8bcd2fb9d30edec8ae5784f6bfaf6
Beatriz Sopeña <beatrizsmerino@gmail.com>
2020-06-09 23:13:25 +0200

    Class 33. Geolocation with Google maps.
    Exercise 4. Find your city on the map
    
    - Changed the icon maker png to svg file and resize it.
    - Bug fixed. Jsdocs documentation images did not load.
    - Generated the documentation Jsdocs.

--- beatrizsmerino/class-33/find-city-google-maps/js/google-maps.js
+++ beatrizsmerino/class-33/find-city-google-maps/js/google-maps.js
@@ -154,8 +154,8 @@ function initMap(coords, mapDom, jsonStyles) {
 */
 function addMarker(contentMarker = null, coords, map) {
 	const iconMarker = {
-		url: './img/location.png', // url
-		scaledSize: new google.maps.Size(50, 50), // scaled size
+		url: './img/icon-location.svg', // url
+		scaledSize: new google.maps.Size(50, 60), // scaled size
 		// origin: new google.maps.Point(0, 0), // origin
 		// anchor: new google.maps.Point(0, 0) // anchor
 	};
